ca1417eaab197babf0ccf45b1757e8b255c45023
lor08 <spike.09@ya.ru>
2017-06-30 15:55:48 +0300

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Carbon\Carbon;
+use Illuminate\Support\ServiceProvider;
+use Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        Carbon::setLocale('ru');
+        setlocale(LC_ALL, 'ru_RU.utf8');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
